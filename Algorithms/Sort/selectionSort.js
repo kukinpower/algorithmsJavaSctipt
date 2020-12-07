aad0c099cb35f9289c96266adcd09b2d8d47a5b7
kukinpower <thekukinpower@gmail.com>
2020-12-07 03:42:31 +0300

    add selection sort

---
+++ Algorithms/Sort/selectionSort.js
@@ -0,0 +1,25 @@
+/**
+ * Selection sort complexity:
+ * n + (n-1) + (n-2) + ... + 1
+ * n(n + 1) / 2
+ * (n^2 + n) / 2
+ * n^2 / 2 + n / 2
+ * O(n^2)
+ */
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let k = i;
+        let tmp;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[k])
+                k = j;
+        }
+        tmp = arr[i];
+        arr[i] = arr[k];
+        arr[k] = tmp;
+    }
+    return arr;
+}
+
+console.log(selectionSort([2, 7, 8, 1, 1, 3]));
